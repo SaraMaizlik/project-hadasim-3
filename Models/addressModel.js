bfbf5395df4e39f299551a33564aa40a4ae968d1
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-06 23:11:44 +0300

    required fields for a address

--- Models/addressModel.js
+++ Models/addressModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const addressSchema = mongoose.Schema({
-    
+    city: {type: String, require},
+    street: {type: String, require},
+    number: {type: Number, require}
 }) 
 
 module.exports = mongoose.model("Address", addressSchema)

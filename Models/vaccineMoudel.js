148dec506019ec1f65d0813c5f5b510d7b8a799d
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-06 23:06:09 +0300

    required fields for a vaccine

--- Models/vaccineMoudel.js
+++ Models/vaccineMoudel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const vaccineSchema = mongoose.Schema({
-    
+    receivingDate: {type: Date, require},
+    manufacturer: {type: String, require}
 }) 
 
 module.exports = mongoose.model("Vaccine", vaccineSchema)

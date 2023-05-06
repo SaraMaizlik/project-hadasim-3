c2d0d5a6d9e876a9cf48445b9e13e99e05719859
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-06 22:56:55 +0300

    required fields for a member

--- Models/memberModel.js
+++ Models/memberModel.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const memberSchema = mongoose.Schema({
-    firstName: {type: String, require,},
-    lastName: {type: String, require,},
+    firstName: {type: String, require},
+    lastName: {type: String, require},
+    personalID: {type: String, require, unique},
+    address: {type: mongoose.Schema.Types.ObjectId,ref:'Address', require},
+    birthDate: {type: Date, require},
+    phone: {type: String, require,},
+    cellPhone: {type: String, require,},
+    vaccine: [{type: mongoose.Schema.Types.ObjectId,ref:'Vaccine'}],
+    positiveAndRecoveryDate: [{type: Date}],
 }) 
 
 module.exports = mongoose.model("Member", memberSchema)

ec1f17d480ef388adcee95b61c26b31a72c65f8a
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-07 12:04:28 +0300

    Adding routing to functions without testing and fixing

--- Routes/vaccineRoute.js
+++ Routes/vaccineRoute.js
@@ -1,7 +1,7 @@
 const router=require('express').Router();
 const vaccine=require("../Controllers/vaccineController")
 
-
-
+router.post('/newVaccine',vaccine.newVaccine);
+router.get('/findVaccineById/:id',vaccine.findVaccineById);
 
 module.exports=router;

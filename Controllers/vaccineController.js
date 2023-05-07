3cbe849036bb3665058a6d4977c2a35103a0c7b1
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-07 11:29:00 +0300

    Creating functions without testing and fixing

--- Controllers/vaccineController.js
+++ Controllers/vaccineController.js
@@ -1,1 +1,27 @@
 const Vaccine = require('../Models/vaccineModel')
+
+const newVaccine = async (req, res) => {
+    let coronaVaccine = new Vaccine({
+        receivingDate: req.body.receivingDate,
+        manufacturer: req.body.manufacturer,
+    }).populate()
+    try {
+        await coronaVaccine.save();
+        res.status(200).json({ newVaccine: coronaVaccine });
+    }
+    catch (error) {
+        res.status(400).send("cannot save new veccine: " + error.message)
+    }
+}
+
+const findVaccineById = async (req, res) => {
+    try {
+        let vaccine = await Vaccine.findById(req.body.id)
+        res.status(200).json({ getVaccineById: vaccine });
+    }
+    catch (error) {
+        res.status(400).send("cannot find the vaccine: " + error.message)
+    }
+}
+
+module.exports = { newVaccine, findVaccineById}

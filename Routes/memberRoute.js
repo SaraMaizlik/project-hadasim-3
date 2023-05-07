006beec1474383036caf7b9b827115d3763405f2
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-07 12:01:44 +0300

    Adding routing to functions without testing and fixing

--- Routes/memberRoute.js
+++ Routes/memberRoute.js
@@ -1,7 +1,8 @@
 const router=require('express').Router();
 const member=require("../Controllers/memberController")
 
-
-
+router.post('/newMember',member.newMember);
+router.get('/findMemberById/:id',member.findMemberById);
+router.get('/getAllMembers',member.getAllMembers);
 
 module.exports=router;

9a9d0729dcebd1144d8a5f44f7517f601d2452d3
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-08 11:20:23 +0300

    One word correction

--- Models/memberModel.js
+++ Models/memberModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const memberSchema = mongoose.Schema({
     firstName: {type: String, require},
     lastName: {type: String, require},
-    personalID: {type: String, require, unique},
+    personalID: {type: String, require, unique: true},
     address: {type: mongoose.Schema.Types.ObjectId,ref:'Address', require},
     birthDate: {type: Date, require},
     phone: {type: String, require,},

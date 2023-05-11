932e8ba429309afc0b296e329ae8b0887ab46d9a
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-11 09:28:10 +0300

    Changing the function to internal

--- Routes/addressRoute.js
+++ Routes/addressRoute.js
@@ -1,7 +1,6 @@
 const router=require('express').Router();
 const address=require("../Controllers/addressController")
 
-router.post('/newAddress',address.newAddress);
 router.get('/findAddressById/:id',address.findAddressById);
 
 module.exports=router;

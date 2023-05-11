0f684c2d419f9dbdb3b3a3becbcc317110e9c1ec
SaraMaizlik <publicprograming.s@gmail.com>
2023-05-11 09:29:12 +0300

    Changing a variable name

--- Models/addressModel.js
+++ Models/addressModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const addressSchema = mongoose.Schema({
     city: {type: String, require},
     street: {type: String, require},
-    number: {type: Number, require}
+    houseNumber: {type: Number, require}
 }) 
 
 module.exports = mongoose.model("Address", addressSchema)
